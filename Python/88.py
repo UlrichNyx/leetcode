546175fddb297907c08348737db2687a3480f10b
unknown <kontogiannisfilippos99@gmail.com>
2024-04-07 23:14:46 +0100

    Solved 88

--- Python/88.py
+++ Python/88.py
@@ -2,6 +2,22 @@
 # URL: https://leetcode.com/problems/merge-sorted-array/description/
 
 class Solution(object):
+    def replace_tail(self, nums1, n):
+        for x in range(len(nums1) - 1, len(nums1) - n - 1, -1):
+            if nums1[x] == 0:
+                nums1[x] = None
+            else:
+                return
+
+
+    def pushback(self, nums1, index, value):
+            previous = nums1[index]
+            nums1[index] = value
+            for x in range(index + 1, len(nums1)):
+                temp = nums1[x]
+                nums1[x] = previous
+                previous = temp
+
     def merge(self, nums1, m, nums2, n):
         """
         :type nums1: List[int]
@@ -10,4 +26,25 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        
+
+        pivot1 = 0
+        pivot2 = 0
+
+        self.replace_tail(nums1, n)
+        while pivot2 < n:
+            print(nums1)
+            if nums1[pivot1] is None:
+                print("It was None")
+                self.pushback(nums1, pivot1, nums2[pivot2])
+                pivot2 += 1
+                pivot1 += 1
+            elif nums1[pivot1] < nums2[pivot2]:
+                print("Pivot1")
+                pivot1 += 1
+                continue
+            else:
+                print("Pivot2")
+                self.pushback(nums1, pivot1, nums2[pivot2])
+                pivot2 += 1
+                pivot1 += 1
+
