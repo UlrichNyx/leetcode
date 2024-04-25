576a1058a596271b9af416eba2d60d03aff4aa03
unknown <kontogiannisfilippos99@gmail.com>
2024-04-25 13:55:40 +0100

    Solved 110.py

--- Python/110.py
+++ Python/110.py
@@ -1,3 +1,16 @@
 # 110. Balanced Binary Tree
 # URL: https://leetcode.com/problems/balanced-binary-tree/
-        
+
+import math
+
+class Solution(object):
+    def isBalanced(self, root):
+        if root is None:
+            return True
+        return abs(self.findDepth(root.left, 0) - self.findDepth(root.right, 0)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def findDepth(self, root, count):
+        if root is None:
+            return count
+        count += 1
+        return max(self.findDepth(root.left, count), self.findDepth(root.right, count))
---
+++ Python/111.py
@@ -0,0 +1,3 @@
+# 111. Balanced Binary Tree
+# URL: https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
