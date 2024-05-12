eaa710f8bafee6ad8e72d04b6aea56f2298c7267
UlrichNyx <kontogiannisfilippos99@gmail.com>
2024-05-12 13:20:59 +0300

    Solved 118

--- Python/118.py
+++ Python/118.py
@@ -1,3 +1,14 @@
 # 118. Pascal's Triangle
 # URL: https://leetcode.com/problems/pascals-triangle/
 
+class Solution(object):
+    def generate(self, numRows):
+        if(numRows == 1):
+            return [[1]]
+        previous_values = self.generate(numRows-1)
+        values = [1]
+        for x in range(1, numRows - 1):
+            values.append(previous_values[-1][x-1] + previous_values[-1][x])
+        values.append(1)
+        return previous_values + [values]
+    
